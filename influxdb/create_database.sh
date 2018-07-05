e9cbdcb330c948e48117f01e1efad4d535d21b7e
pgillich <pgillich@gmail.com>
2018-07-05 23:36:41 +0200

    Dockerfile for remote storage adapter
    From https://github.com/terjesannum/prometheus-influxdb-storage-adapter

--- influxdb/create_database.sh
+++ influxdb/create_database.sh
@@ -9,7 +9,4 @@ else
 	docker exec influxdb /bin/bash -c 'influx -username $INFLUXDB_ADMIN_USER -password $INFLUXDB_ADMIN_PASSWORD -database '$CREATE_DB' -execute "GRANT WRITE ON '$CREATE_DB' TO $INFLUXDB_WRITE_USER"'
 	docker exec influxdb /bin/bash -c 'influx -username $INFLUXDB_ADMIN_USER -password $INFLUXDB_ADMIN_PASSWORD -database '$CREATE_DB' -execute "GRANT READ ON '$CREATE_DB' TO $INFLUXDB_READ_USER"'
 	docker exec influxdb /bin/bash -c 'influx -username $INFLUXDB_ADMIN_USER -password $INFLUXDB_ADMIN_PASSWORD -database '$CREATE_DB' -execute "SHOW RETENTION POLICIES ON '$CREATE_DB'"'
-
-
-
 fi
